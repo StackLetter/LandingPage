d14eb7cfacec6cd18b77ebfda89cf6e2c2babb18
Smasty <martin@smasty.net>
2017-10-03 18:23:47 +0200

    First stab at SE OAuth implementation

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -2,12 +2,70 @@
 
 namespace App\Presenters;
 
-use Nette;
+use GuzzleHttp\Client;
+use Nette\Application\UI;
+use Nette\Http\Url;
+use Tracy\Debugger;
 
 
-class HomepagePresenter extends Nette\Application\UI\Presenter
-{
-    function actionDefault(){
+/**
+ * @property-read array $config
+ */
+class HomepagePresenter extends UI\Presenter{
 
+    public function getConfig(){
+        return $this->context->getParameters();
     }
+
+    protected function createComponentSignUpForm(){
+        $form = new UI\Form;
+
+        $form->addEmail('mail', 'E-mail')->setRequired();
+        $form->addMultiSelect('site', 'Stack Exchange site', $this->config['se_sites'])->setRequired();
+        $form->addSubmit('send', 'Authorize & Sign up');
+        $form->onSuccess[] = [$this, 'signUpFormSubmitted'];
+
+        return $form;
+    }
+
+    public function signUpFormSubmitted(UI\Form $form){
+        $values = (array) $form->values;
+
+        // TODO check if email is in database
+
+        $this->redirectUrl($this->constructOAuthUrl());
+    }
+
+    private function constructOAuthUrl(){
+        $api = $this->config['se_api'];
+
+        $url = new Url($api['base_url']);
+        $url->setQueryParameter('client_id', $api['client_id'])
+            ->setQueryParameter('scope', $api['scope'])
+            ->setQueryParameter('redirect_uri', $this->link('//authorize'));
+        return $url;
+    }
+
+
+    public function actionAuthorize($code){
+        $api = $this->config['se_api'];
+        $http = new Client();
+
+        $res = $http->request('POST', $api['token_url'], [
+            'form_params' => [
+                'client_id' => $api['client_id'],
+                'client_secret' => $api['client_secret'],
+                'code' => $code,
+                'redirect_uri' => $this->link('//authorize')
+            ]
+        ]);
+
+        if($res->getStatusCode() != 200){
+            $this->flashMessage('Could not authorize the application. Please try again later.', 'danger');
+            $this->redirect('default#signup');
+        }
+        $data = json_decode($res->getBody(), true);
+        Debugger::barDump($data);
+    }
+
 }
