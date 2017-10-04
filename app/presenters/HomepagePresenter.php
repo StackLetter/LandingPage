69e3ce82a0dea7af69ff844943794d6494b3e318
Smasty <martin@smasty.net>
2017-10-04 23:27:34 +0200

    use the correct token :D

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -39,13 +39,13 @@ class HomepagePresenter extends UI\Presenter{
     private function constructOAuthUrl(){
         $api = $this->config['se_api'];
 
-        $this->session->token = bin2hex(random_bytes(32));
-        $this->session->setExpiration('10 minutes', 'token');
+        $this->session->csrf = bin2hex(random_bytes(32));
+        $this->session->setExpiration('10 minutes', 'csrf');
 
         $url = new Url($api['base_url']);
         $url->setQueryParameter('client_id', $api['client_id'])
             ->setQueryParameter('scope', $api['scope'])
-            ->setQueryParameter('redirect_uri', $this->link('//authorize', ['token' => $this->session->token]));
+            ->setQueryParameter('redirect_uri', $this->link('//authorize', ['csrf' => $this->session->csrf]));
         return $url;
     }
 
@@ -55,13 +55,13 @@ class HomepagePresenter extends UI\Presenter{
     }
 
 
-    public function actionAuthorize($token, $code){
+    public function actionAuthorize($csrf, $code){
         $showError = function (){
             $this->flashMessage('Could not authorize the application. Please try again later.', 'danger');
             $this->redirect('default#signup');
         };
 
-        if(($this->session->token !== null && $token !== $this->session->token) || !$code){
+        if(($this->session->csrf !== null && $csrf !== $this->session->csrf) || !$code){
             $this->redirect('default');
         }
 
@@ -77,7 +77,7 @@ class HomepagePresenter extends UI\Presenter{
                     'client_id' => $api['client_id'],
                     'client_secret' => $api['client_secret'],
                     'code' => $code,
-                    'redirect_uri' => $this->link('//authorize', ['token' => $this->session->token]),
+                    'redirect_uri' => $this->link('//authorize', ['csrf' => $this->session->csrf]),
                 ],
             ]);
         } catch(ClientException $e){
@@ -122,8 +122,8 @@ class HomepagePresenter extends UI\Presenter{
         }
 
         $this->model->beginTransaction();
-        $account_id = $this->model->createAccount($values['mail'], $this->session->token);
-        $this->model->createUsers($account_id, $values['site'], $this->session->token);
+        $account_id = $this->model->createAccount($values['mail'], $this->session->access_token);
+        $this->model->createUsers($account_id, $values['site'], $this->session->access_token);
         $this->model->commitTransaction();
     }
 
