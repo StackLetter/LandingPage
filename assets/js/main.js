a7516989ffc9abacc609291c375ef90ed024d351
mmatusSVK <matus.salat@gmail.com>
2017-10-14 20:56:43 +0200

    Add href scroll to, work on texts
    * Change texts
    * Add href scroll to, work on texts

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,4 +6,11 @@ $(function(){
         size: 4
     });
 
+    $body.on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top - 60
+        }, 500);
+    });
 });
