4b12508dd360b9990d6403b840445b88f1a25a8e
Smasty <martin@smasty.net>
2017-10-14 16:01:40 +0200

    Process queue: create a PID file

--- app/bin/process-queue.php
+++ app/bin/process-queue.php
@@ -5,8 +5,12 @@ use App\Models\AsyncJobProcessor;
 use Nette\DI\Container;
 use Tracy\Debugger;
 
+
 /** @var Container $container */
 $container = require __DIR__ . '/../bootstrap.php';
+
+// Create PID file
+file_put_contents($container->parameters['tempDir'] . '/process-queue.pid', getmypid());
 
 /** @var AsyncJobProcessor $processor */
 $processor = $container->getByType(AsyncJobProcessor::class);
